65be54b2e75eb7d0f83fa4c8f02624b34898b8ae
Hugo Ferreira <hugofm2@ufmg.br>
2021-06-30 12:11:14 -0300

    Implementacao copy image ipad->pc

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template, redirect, url_for, request, send_file
+from flask import Flask,request, send_file
 
-# import pyperclip as clipboard
 
-from PIL import ImageGrab, ImageChops, Image
-import io, base64, pyperclip
+
+from PIL import ImageGrab, Image
+import io,  pyperclip
+import win32clipboard
+
 
 
 app = Flask(__name__)
+
+
+def send_to_clipboard(clip_type, data):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardData(clip_type, data)
+    win32clipboard.CloseClipboard()
+
 
 @app.route('/', methods=['GET'])
 def getClipboard():
@@ -18,3 +28,26 @@ def getClipboard():
 		return	send_file(buffer_img	, mimetype='image/png')
 	else: # Caso seja um texto
 		return pyperclip.paste()
+
+
+@app.route('/uploader',methods =['POST'])
+def success():
+	if request.method == 'POST':  
+		f = request.files['file']
+		f = request.files.get('file', None)	
+
+		image = Image.open(f)
+
+		output = io.BytesIO()
+		new_image = Image.new("RGBA", image.size, "WHITE") # Colocar fundo branco em pngs transparentes
+		new_image.paste(image, mask=image)
+		new_image.convert("RGB").save(output, "BMP")
+		data = output.getvalue()[14:]
+		output.close()
+		send_to_clipboard(win32clipboard.CF_DIB, data) 
+		return 'Sucesso!'
+
+
+
+	
+
